29ad042b67bfdc1de57def148174a3d4728aba70
Andreas Derksen <andreas.derksen2@gmail.com>
2020-12-11 21:02:36 +0100

    day4 step 1

--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -1,21 +1,46 @@
+import { runMeasured } from "../utils";
 import { inputDay4 } from "./input";
 
 const requiredFields = [
-  "byr", //(Birth Year)
-  "iyr", //(Issue Year)
-  "eyr", //(Expiration Year)
-  "hgt", //(Height)
-  "hcl", //(Hair Color)
-  "ecl", //(Eye Color)
-  "pid", //(Passport ID)
-  "cid", //(Country ID)
+  "byr", // (Birth Year)
+  "iyr", // (Issue Year)
+  "eyr", // (Expiration Year)
+  "hgt", // (Height)
+  "hcl", // (Hair Color)
+  "ecl", // (Eye Color)
+  "pid", // (Passport ID)
+  // "cid", // (Country ID)
 ];
 
-function parseInput(input: string) {
-  const chunks = input.split("\n\n");
-  console.log(chunks);
+interface DataSet {
+  [key: string]: string;
 }
 
-export function day4() {
+function parseInput(input: string): DataSet[] {
+  const chunks = input.split("\n\n");
+  //   console.log(chunks);
+  const x: DataSet[] = chunks.map((c) =>
+    c.split(/\s+/g).reduce((curr, pair) => {
+      const [key, val] = pair.split(":");
+      curr[key] = val;
+      return curr;
+    }, {} as DataSet)
+  );
+  return x;
+}
+
+function isDataSetValid(set: DataSet): boolean {
+  return requiredFields.every((requiredField) => !!set[requiredField]);
+}
+
+function day4_step1() {
   const parsedInput = parseInput(inputDay4);
+  const validSets = parsedInput.filter(isDataSetValid);
+  console.log(`${validSets.length} data sets valid.`);
+}
+
+function day4_step2() {}
+
+export function day4() {
+  runMeasured("Day 4 - Step 1", day4_step1);
 }
